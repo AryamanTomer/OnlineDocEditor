06beecd7bd94ac2a9f996ecdcd7f9335bd514b82
Aryaman Tomer <101023524+AryamanTomer@users.noreply.github.com>
2024-04-12 18:28:09 -0500

    Add project files.

---
+++ OnlineDocEditor/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineDocEditor.Models;
+
+namespace OnlineDocEditor.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Doc> Docs {  get; set; }
+    }
+}
